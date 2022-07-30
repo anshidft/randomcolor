5621b534c2ef6fb6891279f9804deb2e9fd94464
anshidft <anshidp18@gmail.com>
2022-07-30 20:27:12 +0530

    colorpage

---
+++ color.js
@@ -0,0 +1,8 @@
+function changecolor(){
+    document.body.style.backgroundColor='rgb('+Math.round(Math.random()*255)+','+
+    Math.round(Math.random()*255)+','+
+    Math.round(Math.random()*255);
+    document.getElementById('HEAD1').style.color='rgb('+Math.round(Math.random()*215)+','+
+    Math.round(Math.random()*215)+','+
+    Math.round(Math.random()*215);
+}
